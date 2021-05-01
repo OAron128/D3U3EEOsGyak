e8616728c17d729a06c47e3f055a57db1472ef8a
OAron <79246698+OAron128@users.noreply.github.com>
2021-05-01 22:58:06 +0200

    Add files via upload

--- OSSemTask_D3U3EE/beado.c
+++ OSSemTask_D3U3EE/beado.c
@@ -1,25 +1,85 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <sys/file.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	int pipe, vissza;
-	char tomb[32];
+    int vissza;
+    char buffer;
 
-	//O_RDWR / megnyitja a proba txt-t irásra és olvasásra egyaránt.
+    if (argc == 1)
+    {
+        fprintf(stderr, "Használat: %s üzenet\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	pipe=open("proba.txt",O_RDWR);  ///pipe egyenlő lesz a txt megnyitással
-	if (pipe == -1) {
-	    perror("open() hiba!");   ///ha nem sikerül megnyitni vagyis hivás kiirja ezt
-	    exit(-1);
-	}
-	vissza=lseek(pipe,0,SEEK_SET);    //lseek(honnan kezdi el írni) hova teszi a kurzort
-	vissza=read(pipe,tomb,32); // vissza: mennyi bájtot sikerült beolvasni
-	printf("read() olvasott %d byteot, ami a kovetkezo: %s\n",vissza,tomb);
-	close(pipe);
+    // 2 csővezeték:
+    // - szulo_fds: Amit a szülő használ hogy a gyereknek írjon
+    // - gyerek_fds: Amit a gyerek használ hogy a szülőnek írjon
+    int szulo_fds[2], gyerek_fds[2];
+
+    // olvasás:[0] - írás:[1]
+    if (pipe(szulo_fds) != 0 || pipe(gyerek_fds) != 0)
+    {
+        printf("Csövek létrehozása sikertelen!\n");
+        return EXIT_FAILURE;
+    }
+
+    // Gyerek processz forkolás
+    int child = fork();
+
+    if (child < 0)
+    {
+        printf("Sikertelen forkolás!\n");
+        return EXIT_FAILURE;
+    }
+    else if (child == 0)
+    {
+
+        close(gyerek_fds[0]);    
+        close(szulo_fds[1]);
+
+        // Olvasás szülő csővezetékből
+        char szulotol[100];
+        int n = read(szulo_fds[0], szulotol, sizeof(szulotol) - 1);
+        szulotol[n] = '\0';
+        printf("GYEREK - PID = %d: Beolvastam a szülőtől: '%s'\n", (int)getpid(), szulotol);
+
+        close(szulo_fds[0]);
+
+        // Amit olvasott most beírjuk
+        strcat(szulotol, " (hozzáadva)");
+
+        write(gyerek_fds[1], szulotol, strlen(szulotol));
+        close(gyerek_fds[1]);
+        printf("GYEREK - PID = %d: Írás a csővezetékre: '%s'\n", (int)getpid(), szulotol);
+        
+    }
+    else
+    {
+        // Lezárom a csővezetékeket
+        close(szulo_fds[0]);
+        close(gyerek_fds[1]);
+
+        // Terminálból beolvasott szöveg írása a szülő vezetékbe hogy a gyerek kiolvassa
+        printf("SZÜLŐ - PID = %d: Írás a csővezetékre: '%s'\n\n", (int)getpid(), argv[1]);
+        
+        
+        vissza = write(szulo_fds[1], argv[1], strlen(argv[1]));
+        close(szulo_fds[1]);
+		
+		
+        // Gyerek csővezetékből olvasás
+        char gyerektol[100];
+        int n = read(gyerek_fds[0], gyerektol, sizeof(gyerektol) - 1);
+        gyerektol[n] = '\0';
+
+        printf("A write() %d byteot írt.\n\n", vissza);
+
+        close(gyerek_fds[0]);
+        printf("SZÜLŐ - PID = %d: Csővezetékről olvasott: - '%s'\n", (int)getpid(), gyerektol);
+    }
+    return 0;
 }
